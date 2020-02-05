a8a7488773cfc8ea394f7ca23e5e06eeace75e75
Alex Irvine <Alex.b.Irvine@gmail.com>
2020-02-05 07:41:47 -0500

    Created base app & made OutcomeTable

---
+++ components/OutcomeTable.vue
@@ -0,0 +1,18 @@
+<template>
+  <table class="text-center md:mw-100 md:mx-auto">
+    <!-- Apply Tailwind CSS  -->
+    <tr v-for="(outcome, index) in outcomes" :key="index">
+      <td>{{ index + 1 }}</td>
+      <td>{{ outcome }}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "OutcomeTable",
+  props: {
+    outcomes: Array
+  }
+};
+</script>
