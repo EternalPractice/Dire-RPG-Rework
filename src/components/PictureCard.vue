494e07ab4d5083835eb95f5ccb649c054a524b73
Alex Irvine <alex.b.irvine@gmail.com>
2020-02-17 02:08:48 -0500

    hi alex

---
+++ src/components/PictureCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="PictureCard">
+    <img :src="pictureSource" alt="<slot />" />
+    <p><slot /></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PictureCard",
+  props: {
+    pictureSource: String,
+    text: String
+  }
+};
+</script>
+
+<style>
+.PictureCard {
+  /*Keeps square shape*/
+  height: 0;
+  width: 20vw;
+  padding-bottom: 20vw;
+
+  background: blue;
+}
+
+img {
+  max-width: 75%;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -16,12 +16,21 @@
       <li>Make layout for the 9 schools</li>
       <li>Create components for cards (picture only, stats with picture)</li>
     </ul>
+
+    <PictureCard
+      pictureSource="https://i.ytimg.com/vi/WyOzKG3OoBg/hqdefault.jpg"
+    >
+      This is a birb
+    </PictureCard>
   </div>
 </template>
 
 <script>
+import PictureCard from "@/components/PictureCard.vue";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: { PictureCard }
 };
 </script>
 
